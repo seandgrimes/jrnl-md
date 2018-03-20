37d05008a09f0715efbf072b65620051cbdf6ab8
Sean Grimes <sean@codemunkey.net>
2018-03-20 12:32:16 -0500

    Fixing JSON serialization/deserialization issues
    First off, the native Map type in ES6 doesn't serialize to JSON,
    so replaced that with a regular object that could be used as a
    dictionary.
    
    Second off, methods/functions don't serialize to JSON either, so if we
    want our methods+data to be colocated for TreeNode, we need to
    rehydrate to actual TreeNode instances when we load ourself from
    JSON

--- src/storage/tree-node.ts
+++ src/storage/tree-node.ts
@@ -1,25 +1,34 @@
 import {Entry} from './entry';
 
+interface Dictionary {
+  [propName: string]: TreeNode;
+}
+
+interface JsonEntry {
+  json: TreeNode,
+  node: TreeNode
+};
+
 export class TreeNode {
-  private index: Map<string, TreeNode> = new Map<string, TreeNode>();
+  private index: Dictionary = {};
   key: string;
   keys: string[] = [];
   value: Entry;
 
   get children() : TreeNode[] {
     // TODO: Use a generator
-    return this.keys.map(key => this.index.get(key));
+    return this.keys.map(key => this.index[key]);
   }
 
-  constructor(key: string, value: Entry) {
+  constructor(key: string = null, value: Entry = null) {
     this.key = key;
     this.value = value;
   }
 
   addChild(node: TreeNode) {
-    if (this.index.has(node.key)) throw `Index '${node.key}' already exists`;
+    if (this.index[node.key]) throw `Index '${node.key}' already exists`;
 
-    this.index.set(node.key, node);
+    this.index[node.key] = node;
     this.keys.push(node.key);
     this.keys.sort();
   }
@@ -35,6 +44,30 @@ export class TreeNode {
   }
 
   findChild(key: string) : TreeNode {
-    return this.index.get(key) || null;
+    return this.index[key] || null;
+  }
+
+  static fromJson(json : string) : TreeNode {
+    // This is a fiction, we have no guarantee that the shape of the data is what
+    // we expect, we also can't call any TreeNode methods on this either. It does
+    // let the compiler catch some errors though
+    const deserialized = JSON.parse(json) as TreeNode;
+
+    const root = new TreeNode();
+    const stack: JsonEntry[] = [];
+    stack.push({json: deserialized, node: root});
+
+    while (stack.length > 0) {
+      const {node: parent, json} = stack.pop();
+
+      json.keys.forEach(key => {
+        const temp = json.index[key] as TreeNode;
+        const child = new TreeNode(temp.key, temp.value);
+        parent.addChild(child);
+        stack.push({json: temp, node: child});
+      });
+    }
+
+    return root;
   }
 }
