2cd68a6bf206ffb7d47a359556418a7def02a395
Sean Grimes <sean@codemunkey.net>
2018-03-23 10:29:22 -0500

    Editing journal entries causes dup date line (#14)
    * Editing journal entries causes dup date line
    
    This fixes #12. In a nutshell, we were always creating a temp
    file when adding or editing a journal by concatenating the journal
    entry date with the body. This meant we were doubling, tripling,
    quadrupling, etc. the journal entry date on the first line each
    time we edited the file.
    
    Simple fix, just make the code for writing the journal entry to the
    temp file for editing aware of whether a new journal entry or an
    existing one is being edited.
    
    * Refactoring temp file logic into new class
    
    This is to make testing a little easier, need to refactor
    EditorService more for testability but that will have to be a task
    for later
    
    * Removing tempfile-parser.ts
    
    This got merged into the source file for the new
    JournalTempFile class
    
    * Removing unused import statements

--- src/editor/editor-service.ts
+++ src/editor/editor-service.ts
@@ -1,14 +1,11 @@
-import * as fs from 'fs';
 import * as moment from 'moment';
-import * as os from 'os';
 import * as process from 'process';
-import * as uniqueFilename from 'unique-filename';
 import {injectable} from 'inversify';
 import {spawn} from 'child_process';
 import {Entry} from '../storage/entry';
-import {TempFileParser} from './tempfile-parser';
+import { JournalTempFile } from './journal-temp-file';
 
-/** 
+/**
  * Editor service for interacting with the user's
  * default text editor to create and edit existing
  * journal entries
@@ -16,109 +13,68 @@ import {TempFileParser} from './tempfile-parser';
 @injectable()
 export class EditorService {
   private editor: string;
-  private tmpDir: string;
 
-  /** 
+  /**
    * Constructor
   */
   constructor() {
     this.editor = process.env.EDITOR;
-    this.tmpDir = os.tmpdir();
   }
 
-  /** 
-   * Opens the user's default editor to create a new journal entry and then 
+  /**
+   * Opens the user's default editor to create a new journal entry and then
    * converts the temp file to a new journal entry once the editor closes
-   * 
+   *
    * @returns A promise containing the new journal entry or null when it is resolved
   */
   createJournalEntry() : Promise<Entry> {
-    const tempFile = this.generateTempFile();
+    const tempFile = new JournalTempFile();
     const entry: Entry = {
       date: moment().format(),
       body: ''
     };
 
-    return this.spawnEditor(tempFile, entry);
+    return this.spawnEditor(tempFile, entry, true);
   }
 
-  /** 
-   * Opens the user's default editor to edit an existing journal entry and then 
+  /**
+   * Opens the user's default editor to edit an existing journal entry and then
    * converts the temp file to a new journal entry once the editor closes
-   * 
+   *
    * @returns A promise containing the new journal entry or null when it is resolved
   */
   editJournalEntry(entry: Entry) : Promise<Entry> {
-    const tempFile = this.generateTempFile();
-    return this.spawnEditor(tempFile, entry);
-  }
-
-  /** 
-   * Generates a unique temp file name for use when creating
-   * or editing existing journal entries
-   * 
-   * @returns A unique temp file name
-  */
-  private generateTempFile() : string {
-    return uniqueFilename(this.tmpDir) + '.md';
+    const tempFile = new JournalTempFile();
+    return this.spawnEditor(tempFile, entry, false);
   }
 
   /**
    * Opens the specified temp file in the user's default editor
-   * 
+   *
    * @param tempFile The temp file to open in the user's editor
+   * @param entry The entry to edit, can be a new or existing entry
+   * @param isNewEntry Whether or not this is a new or existing entry
    * @returns A promise with the parsed Entry from the temp file
    */
-  private spawnEditor(tempFile: string, entry: Entry) : Promise<Entry> {
+  private spawnEditor(tempFile: JournalTempFile, entry: Entry, isNewEntry: boolean) : Promise<Entry> {
     const openEditor = () => {
       const editorParts = this.editor.split(' ').filter(entry => entry.trim() !== '');
       const editorCmd = editorParts.shift();
       const args = editorParts || [];
-      
+
       return new Promise<void>((resolve, reject) => {
-        args.push(tempFile);
-        const editor = spawn(editorCmd, args);      
+        args.push(tempFile.filename);
+        const editor = spawn(editorCmd, args);
         editor.on('close', (code) => {
-          code !== 0 
-            ? reject(`Editor exited with code ${code}`) 
+          code !== 0
+            ? reject(`Editor exited with code ${code}`)
             : resolve();
         });
       });
     }
 
-    const readTempFile = () => { 
-      return new Promise<Entry>((resolve, reject) => {
-        fs.readFile(tempFile, 'utf8', (err, data) => {
-          if (!err) {
-            const entry = TempFileParser.parse(data);
-            resolve(entry);
-            return;
-          }
-
-          reject(err);
-        });
-      });
-    }
-
-    return this.writeToTempFile(tempFile, entry)
+    return tempFile.write(entry, isNewEntry)
       .then(() => openEditor())
-      .then(() => readTempFile());
-  }
-
-  /**
-   * Writes the specified journal entry to the specified
-   * temp file
-   * 
-   * @param tempFile The temp file to write the entry to
-   * @param entry The entry to write to the temp file
-   * @returns A promise that is resolved once the write is complete and rejected on error
-   */
-  private writeToTempFile(tempFile: string, entry: Entry) : Promise<string> {
-    return new Promise<string>((resolve, reject) => {
-      const contents = `${entry.date}\n${entry.body}`;
-      fs.writeFile(tempFile, contents, { flag: 'w' },  (err) => {
-        err ? reject(err) : resolve(tempFile);
-      });
-    });
+      .then(() => tempFile.read());
   }
 }
---
+++ src/editor/journal-temp-file.specs.ts
@@ -0,0 +1,64 @@
+import 'mocha';
+import * as moment from 'moment';
+import {expect} from 'chai';
+import {Entry} from '../storage/entry';
+import {JournalTempFile} from './journal-temp-file';
+
+describe('JournalTempFile', () => {
+  describe('When writing a journal entry to the temp file', () => {
+
+    it ("the journal entry date isn't added to the file twice on edit", async () => {
+      // Arrange
+      const tempFile = new JournalTempFile();
+      const now = moment().toISOString();
+      const entry = {
+        date: now,
+        body: `${now}\nThis is a test`
+      };
+
+      // Act
+      await tempFile.write(entry, false);
+      const written = await tempFile.read();
+      const bodyLines = written.body.split('\n');
+
+      // Assert
+      expect(bodyLines[0]).not.to.be.equal(bodyLines[1]);
+    });
+
+    it('the journal date is written to the file for new entries', async () => {
+      // Arrange
+      const tempFile = new JournalTempFile();
+      const now = moment().toISOString();
+      const entry = {
+        date: now,
+        body: 'This is a test'
+      };
+
+      // Act
+      await tempFile.write(entry, true);
+      const written = await tempFile.read();
+
+      // Assert
+      expect(now).to.equal(written.date);
+    });
+
+    it('the journal body is written to the file', async () => {
+      // Arrange
+      const tempFile = new JournalTempFile();
+      const entry = {
+        date: moment().toISOString(),
+        body: 'This is a test'
+      };
+
+      // Should probably change this behavior, but for now...
+      const expected = `${entry.date}\n${entry.body}`;
+
+      // Act
+      await tempFile.write(entry, true);
+      const written = await tempFile.read();
+
+      // Assert
+      expect(expected).to.equal(written.body);
+    });
+  });
+});
